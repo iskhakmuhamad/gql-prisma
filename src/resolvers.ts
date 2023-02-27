79051237a4c35d9dfda19212c79edd6df93cbc73
iskhakmuhamad <iskhakmuhama@gmail.com>
2023-02-28 06:38:34 +0700

    add author

--- src/resolvers.ts
+++ src/resolvers.ts
@@ -12,34 +12,65 @@ export const resolvers = {
       },
     },
     Mutation: {
-      createDraft: (parent, args) => {
-        return prisma.post.create({
-          data: {
-            title: args.title,
-            content: args.content,
+        createDraft: (parent, args) => {
+            return prisma.post.create({
+              data: {
+                title: args.title,
+                content: args.content,
+                published: false,
+                author: args.authorEmail && {
+                  connect: { email: args.authorEmail },
+                },
+              },
+            })
           },
-        })
-      },
-      publish: (parent, args) => {
-        return prisma.post.update({
-          where: {
-            id: Number(args.id),
+          publish: (parent, args) => {
+            return prisma.post.update({
+              where: { id: Number(args.id) },
+              data: {
+                published: true,
+              },
+            })
           },
-          data: {
-            published: true,
+          createUser: (parent, args) => {
+            return prisma.user.create({
+              data: {
+                email: args.data.email,
+                name: args.data.name,
+                posts: {
+                  create: args.data.posts,
+                },
+              },
+            })
           },
-        })
-      },
-      delete: async (parent, args) => {
-        const deleted = await prisma.post.delete({
-            where: {
-              id: Number(args.id),
-            }
-          })
-        if (!deleted) {
-          return 'Failed delete the article with id : ' + args.id
-        } 
-        return 'Success delete the article with id : ' + args.id
-      },
+          delete: async (parent, args) => {
+            const deleted = await prisma.post.delete({
+                where: {
+                  id: Number(args.id),
+                }
+              })
+            if (!deleted) {
+              return 'Failed delete the article with id : ' + args.id
+            } 
+            return 'Success delete the article with id : ' + args.id
+          },
+        },
+        User: {
+          posts: (parent, args) => {
+            return prisma.user
+              .findUnique({
+                where: { id: parent.id },
+              })
+              .posts()
+          },
+        },
+        Post: {
+          author: (parent, args) => {
+            return prisma.post
+              .findUnique({
+                where: { id: parent.id },
+              })
+              .author()
+          },
     },
 }
--- src/schema.ts
+++ src/schema.ts
@@ -1,11 +1,19 @@
 const { gql } = require('apollo-server')
 
 export const typeDefs = gql`
+  type User {
+    email: String!
+    id: ID!
+    name: String
+    posts: [Post!]!
+  }
+
   type Post {
     content: String
     id: ID!
     published: Boolean!
     title: String!
+    author: User
   }
 
   type Query {
@@ -14,8 +22,21 @@ export const typeDefs = gql`
   }
 
   type Mutation {
-    createDraft(content: String, title: String!): Post!
+    createUser(data: UserCreateInput!): User!
+    createDraft(authorEmail: String, content: String, title: String!): Post!
     publish(id: ID!): Post
     delete(id: ID!): String!
+  }
+
+  input UserCreateInput {
+    email: String!
+    name: String
+    posts: [PostCreateWithoutAuthorInput!]
+  }
+
+  input PostCreateWithoutAuthorInput {
+    content: String
+    published: Boolean
+    title: String!
   }
 `
